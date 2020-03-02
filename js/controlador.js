ec01c421cf4742ecf34228aba8c2b96e69f854d8
Eduardo Lopez <eduardolopezlainez2001@gmail.com>
2020-03-01 23:48:05 -0600

    Adding Business Form

---
+++ js/controlador.js
@@ -0,0 +1,9 @@
+function showBusinessForm(){
+    document.getElementById('userForm').style.display = 'none';
+    document.getElementById('businessForm').style.display = 'block';
+}
+
+function showUserForm(){
+    document.getElementById('userForm').style.display = 'block';
+    document.getElementById('businessForm').style.display = 'none';
+}
