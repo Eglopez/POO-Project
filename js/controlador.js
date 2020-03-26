bb3607a5315f7109dcead58dac958438eabf0bb8
Eduardo Lopez <eduardolopezlainez2001@gmail.com>
2020-03-26 14:05:11 -0600

    Adding User and BUsiness json

--- js/controlador.js
+++ js/controlador.js
@@ -1,3 +1,69 @@
+users = [
+  {
+    name:"Eduardo",
+    lastname:"Lopez",
+    username:"Eduardo Lopez",
+    email:"eduardolopezlainez2001@gmail.com",
+    password:"asd.123",
+    phone_number:"+504 9853-6822",
+    sex:"male"
+  },
+  {
+    name:"Rosa",
+    lastname:"Sanchez",
+    username:"Rosa",
+    email:"rosanchz@gmail.com",
+    password:"asd.456",
+    phone_number:"+504 9822-3689",
+    sex:"female"
+  },
+  {
+    name:"Mario",
+    lastname:"Fernandez",
+    username:"Mafer",
+    email:"mafer99@gmail.com",
+    password:"asd.789",
+    phone_number:"+504 3422-7681",
+    sex:"male"
+  }
+]
+
+business = [
+  {
+    name:"Jetstereo",
+    acrony:"JS",
+    email:"jetstereo@gmail.com",
+    adress:"Col.Centroamerica",
+    longitud_latitude:"15° 38°",
+    country:"Honduras",
+    state:"Francisco Morazan",
+    zip_code:"	11101",
+    logo:"../img/1.jpg",
+    img:"../img/jetstereo.jpeg",
+    products:[
+      {
+        name:"Muebles",
+        promo:"Los mejores muebles para tu hogar",
+        amount:"15 unidades",
+        previous_price:"L.30,000",
+        price:"L.26,455",
+        category:"Hogar",
+        img:"../img/mueble.jpg"
+      },
+    ]
+  }
+]
+
+
+
+
+
+
+
+
+
+
+
 function showBusinessForm(){
     document.getElementById('userForm').style.display = 'none';
     document.getElementById('businessForm').style.display = 'block';
