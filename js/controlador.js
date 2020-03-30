2bb0c5fb9863c27ae74e9f11c76434bb998ec8db
Eduardo Lopez <eduardolopezlainez2001@gmail.com>
2020-03-29 18:20:29 -0600

    Starting home and profile js

--- js/controlador.js
+++ js/controlador.js
@@ -1,65 +1,3 @@
-users = [
-  {
-    name:"Eduardo",
-    lastname:"Lopez",
-    username:"Eduardo Lopez",
-    email:"eduardolopezlainez2001@gmail.com",
-    password:"asd.123",
-    phone_number:"+504 9853-6822",
-    sex:"male"
-  },
-  {
-    name:"Rosa",
-    lastname:"Sanchez",
-    username:"Rosa",
-    email:"rosanchz@gmail.com",
-    password:"asd.456",
-    phone_number:"+504 9822-3689",
-    sex:"female"
-  },
-  {
-    name:"Mario",
-    lastname:"Fernandez",
-    username:"Mafer",
-    email:"mafer99@gmail.com",
-    password:"asd.789",
-    phone_number:"+504 3422-7681",
-    sex:"male"
-  }
-]
-
-business = [
-  {
-    name:"Jetstereo",
-    acrony:"JS",
-    email:"jetstereo@gmail.com",
-    adress:"Col.Centroamerica",
-    longitud_latitude:"15° 38°",
-    country:"Honduras",
-    state:"Francisco Morazan",
-    zip_code:"	11101",
-    logo:"../img/1.jpg",
-    img:"../img/jetstereo.jpeg",
-    products:[
-      {
-        name:"Muebles",
-        promo:"Los mejores muebles para tu hogar",
-        amount:"15 unidades",
-        previous_price:"L.30,000",
-        price:"L.26,455",
-        category:"Hogar",
-        img:"../img/mueble.jpg"
-      },
-    ]
-  }
-]
-
-
-
-
-
-
-
 
 
 
@@ -74,56 +12,8 @@ function showUserForm(){
     document.getElementById('businessForm').style.display = 'none';
 }
 
-/* globals Chart:false, feather:false */
 
-(function () {
-    'use strict'
-  
-    feather.replace()
-  
-    // Graphs
-    var ctx = document.getElementById('myChart')
-    // eslint-disable-next-line no-unused-vars
-    var myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: [
-          'Sunday',
-          'Monday',
-          'Tuesday',
-          'Wednesday',
-          'Thursday',
-          'Friday',
-          'Saturday'
-        ],
-        datasets: [{
-          data: [
-            15339,
-            21345,
-            18483,
-            24003,
-            23489,
-            24092,
-            12034
-          ],
-          lineTension: 0,
-          backgroundColor: 'transparent',
-          borderColor: '#007bff',
-          borderWidth: 4,
-          pointBackgroundColor: '#007bff'
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: false
-            }
-          }]
-        },
-        legend: {
-          display: false
-        }
-      }
-    })
-  }())
+
+
+ 
+ 
---
+++ js/home.js
@@ -0,0 +1,251 @@
+var users = [
+    {
+      name:"Eduardo",
+      lastname:"Lopez",
+      username:"Eduardo Lopez",
+      email:"eduardolopezlainez2001@gmail.com",
+      password:"asd.123",
+      phone_number:"+504 9853-6822",
+      sex:"male"
+    },
+    {
+      name:"Rosa",
+      lastname:"Sanchez",
+      username:"Rosa",
+      email:"rosanchz@gmail.com",
+      password:"asd.456",
+      phone_number:"+504 9822-3689",
+      sex:"female"
+    },
+    {
+      name:"Mario",
+      lastname:"Fernandez",
+      username:"Mafer",
+      email:"mafer99@gmail.com",
+      password:"asd.789",
+      phone_number:"+504 3422-7681",
+      sex:"male"
+    }
+  ];
+  
+  var business = [
+    {
+      name:"Jetstereo",
+      acrony:"JS",
+      email:"jetstereo@gmail.com",
+      adress:"Col.Centroamerica",
+      longitud_latitude:"15° 38°",
+      country:"Honduras",
+      state:"Francisco Morazan",
+      zip_code:"11101",
+      logo:"../img/1.jpg",
+      img:"../img/jetstereo.jpeg",
+      products:[
+        {
+          name:"Muebles",
+          promo:"Los mejores muebles para tu hogar",
+          amount:"15 unidades",
+          previous_price:"L.30,000",
+          price:"L.26,455",
+          category:"Hogar",
+          img:"../img/mueble.jpg"
+        },
+      ]
+    },
+    {
+      name:"Pizza Hut",
+      acrony:"PH",
+      email:"pizzahut@gmail.com",
+      adress:"Col.Centroamerica",
+      longitud_latitude:"15° 38°",
+      country:"Honduras",
+      state:"Francisco Morazan",
+      zip_code:"11101",
+      logo:"../img/1.jpg",
+      img:"../img/pizzahut.jpeg",
+      products:[
+        {
+          name:"Pizza Hut chesee",
+          promo:"15% de descuento dias Miercoles",
+          amount:"200 unidades",
+          previous_price:"L.500",
+          price:"L.425",
+          category:"Comida",
+          img:"../img/pizza.jpg"
+        },
+      ]
+    },
+    {
+      name:"Diunsa",
+      acrony:"DS",
+      email:"diunsa@gmail.com",
+      adress:"Col.Centroamerica",
+      longitud_latitude:"15° 38°",
+      country:"Honduras",
+      state:"Francisco Morazan",
+      zip_code:"11101",
+      logo:"../img/1.jpg",
+      img:"../img/diunsa.jpeg",
+      products:[
+        {
+          name:"Balon de futbol",
+          promo:"Balon de futbol de la mejor calidad",
+          amount:"120 unidades",
+          previous_price:"L.200",
+          price:"L.90",
+          category:"Deportes",
+          img:"../img/pelota.jpg"
+        },
+      ]
+    }
+  ];
+  
+  var categories = [
+    {
+        category:"Hogar",
+        business:[
+            {
+                name:"Jetstereo",
+                products:[
+                    {
+                        name: "Muebles",
+                        prom: "Los mejores muebles para tu hogar",
+                        previous_price:"L.30,000",
+                        price:"L.26,455",
+                        img: "../img/muebles.jpg"
+                    },
+                    {
+                      name: "Muebles",
+                      prom: "Los mejores muebles para tu hogar",
+                      previous_price:"L.30,000",
+                      price:"L.26,455",
+                      img: "../img/muebles.jpg"
+                    }
+                    
+                ]
+            },
+            {
+                name: "Empresa 2",
+                products:[
+                    {
+                        name: "Muebles",
+                        prom: "Los mejores muebles para tu hogar",
+                        previous_price:"L.30,000",
+                        price:"L.26,455",
+                        img: "../img/muebles.jpg"
+                    },
+                    {
+                      name: "Muebles",
+                      prom: "Los mejores muebles para tu hogar",
+                      previous_price:"L.30,000",
+                      price:"L.26,455",
+                      img: "../img/muebles.jpg"
+                  }
+                ]
+            },
+  
+        ]
+    },
+    {
+      category: "Deportes",
+      business:[
+        {
+          name: "Diunsa",
+          products:[
+            {
+              name:"Balon de futbol",
+              prom:"Balon de futbol de la mejor calidad",
+              previous_price:"L.300",
+              price:"L.95",
+              img: "../img/pelota.jpg"
+            },
+            {
+              name:"Balon",
+              prom:"Balon de futbol de la mejor calidad",
+              previous_price:"L.300",
+              price:"L.95",
+              img: "../img/pelota.jpg"
+            }
+          ] 
+  
+        },
+        {
+          name: "Sports",
+          products:[
+            {
+              name:"Balon de futbol",
+              prom:"Balon de futbol de la mejor calidad",
+              previous_price:"L.300",
+              price:"L.95",
+              img: "../img/pelota.jpg"
+            },
+            {
+              name:"Balon",
+              prom:"Balon de futbol de la mejor calidad",
+              previous_price:"L.300",
+              price:"L.95",
+              img: "../img/pelota.jpg"
+            }
+          ]  
+        }
+      ]
+    }
+  ];
+
+  function userValidation(){
+    for(let i=0;i<users.length;i++){
+      if(users[i].username==document.getElementById('username').value && users[i].password==document.getElementById('password').value){
+        console.log(users[i]);
+        
+        
+        document.getElementById('btn-log').style.display = 'none';
+        document.getElementById('name').innerHTML = `
+        <li class="nav-item dropdown" id="user-name">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-user">${users[i].username}</i>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink" id="user">
+            <a class="dropdown-item" href="user-profile.html"><i class="fas fa-user text-danger"></i>Perfil</a>
+            <a class="dropdown-item" href="#"><i class="fas fa-heart text-danger"></i>Favoritos</a>
+            <a class="dropdown-item" href="#"><i class="fas fa-cart-arrow-down text-danger text-danger"></i>Compras</a>
+            <a class="dropdown-item" href="../index.html"><i class="fas fa-sign-out-alt text-danger"></i>Cerrar sesion</a>
+          </div>
+        </li>
+        `;
+        document.getElementById('home').innerHTML = '';
+        for(let j=0;j<categories.length;j++){
+          if(categories[i].name== 'Hogar'){
+            for(let k=0;k<categories[i].business.length;k++){
+              var busi = categories[i].business;
+              for(let l=0;l<busi[k].products;l++){
+                var product = busi[k].products;
+                document.getElementById('home').innerHTML +=
+                `
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12">
+                  <div class="card shadow bg-white rounded">
+                      <img src="${product.img}" class="card-img-top">
+                      <div class="card-body">
+                          <ul>
+                              <li>${product.prom}</li>
+                              <li id="business">
+                                  ${busi.name}
+                              </li>
+                              <li><i class="fas fa-tags"></i>30 disponibles</li>
+                              <li>${product.previous_price}</li>
+                              <li id="price">${product.price}</li>
+                          </ul>
+                      </div>
+                  </div>
+                </div>
+                `;
+              }
+            }
+          }
+        }
+        $('#loginModal').modal('hide');        
+        
+      }else{
+        $('#loginModal').modal('show');
+      }
+    }
+  }
