56e332ab6d874a3828a688292a4ebdd7b4cd949a
Eduardo Lopez <eduardolopezlainez2001@gmail.com>
2020-04-26 14:25:40 -0600

    Starting the Backend

---
+++ Backend/class/user-class.php
@@ -0,0 +1,149 @@
+<?php
+
+class User{
+    private $id;
+    private $name;
+    private $last_name;
+    private $user_name;
+    private $email;
+    private $password;
+
+
+    function __construct(
+        $id,
+        $name,
+        $last_name,
+        $user_name,
+        $email,
+        $password,
+    ){
+        $this->id = $id;
+        $this->name = $name;
+        $this->last_name = $last_name;
+        $this->user_name = $user_name;
+        $this->emal = $email;
+        $this->password = $password;
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of last_name
+     */ 
+    public function getLast_name()
+    {
+        return $this->last_name;
+    }
+
+    /**
+     * Set the value of last_name
+     *
+     * @return  self
+     */ 
+    public function setLast_name($last_name)
+    {
+        $this->last_name = $last_name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of user_name
+     */ 
+    public function getUser_name()
+    {
+        return $this->user_name;
+    }
+
+    /**
+     * Set the value of user_name
+     *
+     * @return  self
+     */ 
+    public function setUser_name($user_name)
+    {
+        $this->user_name = $user_name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */ 
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */ 
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
+
+?>
