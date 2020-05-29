2b7e7df465900e118a63c42274d3cefa4040536f
Eglopez <eduardolopezlainez2001@gmail.com>
2020-05-28 21:12:53 -0600

    cors test2

--- Backend/api/users.php
+++ Backend/api/users.php
@@ -2,8 +2,11 @@
 
     
     header("Content-Type: application/json");
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, POST');
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Credentials: true");
+    header("Access-Control-Max-Age: 1000");
+    header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+    header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
     include_once('../class/user-class.php');
     require_once('../class/database-class.php');
 
